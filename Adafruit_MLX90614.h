58aae3b5ab4de64ee5f796880da7e07750a733f6
Charlie Harvey <75092607+Geniezito@users.noreply.github.com>
2020-11-27 12:59:18 -0500

    Update Adafruit_MLX90614.h

--- Adafruit_MLX90614.h
+++ Adafruit_MLX90614.h
@@ -63,7 +63,7 @@ class Adafruit_MLX90614 {
   void writeEmissivity(double emissivity);
 
 private:
-  double _T_adjust = 0;
+  double _T_adjust;
   float readTemp(uint8_t reg);
   uint16_t read16(uint8_t addr);
   void write16(uint8_t addr, uint16_t data);
