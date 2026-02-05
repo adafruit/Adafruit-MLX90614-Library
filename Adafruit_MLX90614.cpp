8515e419c2f9b22a666fb14d175e55dfb05995b6
piclaw <piclaw@localhost>
2026-02-05 09:57:01 -0500

    Add clang-format CI job and apply formatting

--- Adafruit_MLX90614.cpp
+++ Adafruit_MLX90614.cpp
@@ -29,7 +29,7 @@ Adafruit_MLX90614::~Adafruit_MLX90614() {
  * @param wire Pointer to Wire instance
  * @return True if the device was successfully initialized, otherwise false.
  */
-bool Adafruit_MLX90614::begin(uint8_t addr, TwoWire *wire) {
+bool Adafruit_MLX90614::begin(uint8_t addr, TwoWire* wire) {
   _addr = addr; // needed for CRC
   if (i2c_dev)
     delete i2c_dev;
@@ -138,7 +138,7 @@ uint16_t Adafruit_MLX90614::read16(uint8_t a) {
   return uint16_t(buffer[0]) | (uint16_t(buffer[1]) << 8);
 }
 
-byte Adafruit_MLX90614::crc8(byte *addr, byte len)
+byte Adafruit_MLX90614::crc8(byte* addr, byte len)
 // The PEC calculation includes all bits except the START, REPEATED START, STOP,
 // ACK, and NACK bits. The PEC is a CRC-8 with polynomial X8+X2+X1+1.
 {
